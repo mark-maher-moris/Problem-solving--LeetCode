b82e166adfa7b84bd62a53f85f477f2c993cd79f
Mark Maher <66829236+mark-maher-moris@users.noreply.github.com>
2025-01-22 01:07:00 +0200

    Time: 5 ms (70.79%) | Memory: 52.7 MB (52.81%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.ts
@@ -0,0 +1,13 @@
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+     const sorted  = [...nums].sort((a,b)=>a-b);
+     const map = new Map<number,number>();
+    for(let i = 0 ;  i< sorted.length;  i ++ ){
+    
+        if(!map.has(sorted[i])){
+           map.set(sorted[i],i);
+        }
+
+    }
+    return nums.map( e=>map.get(e));
+
+}
